e760c61a068b49e1cc17fa9000ab3d56c4243549
Plutraser <plutraser@gmail.com>
2026-07-05 13:25:31 -0500

    Coin spawner implemented
    Coins bronze, silver and gold now spawn on the board in randomized places.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    private int bronzeCoinPoints = 1;
+    private int silverCoinPoints = 3;
+    private int goldCoinPoints = 5;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
--- Assets/Scripts/CoinSpawner.cs
+++ Assets/Scripts/CoinSpawner.cs
@@ -2,15 +2,44 @@ using UnityEngine;
 
 public class CoinSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject[] CoinPrefabs;
+
+    private int initialCoins = 5;
+    private int maxCoins; //Assign this later. i will need to keep track of how many coins are out but rn theres no way to get rid of them...
+
+    private float spawnLimitXLeft = -8f;
+    private float spawnLimitXRight = 8f;
+    private float spawnLimitZUp = 7.8f;
+    private float spawnLimitZDown = -8.4f;
+    private float yPos = .75f;
+
+    private float spawnDelay = 10f;
+    private float spawnInterval;
+
+    /// <summary>
+    /// 
+    /// </summary>
     void Start()
     {
-        
+        for (int i = 0; i < initialCoins; i++)
+        {
+            int initialCoinsRandomization = Random.Range(0, CoinPrefabs.Length);
+            Vector3 initialCoinSpawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), yPos, Random.Range(spawnLimitZDown, spawnLimitZUp));
+
+            Instantiate(CoinPrefabs[initialCoinsRandomization], initialCoinSpawnPos, CoinPrefabs[initialCoinsRandomization].transform.rotation);
+        }
+        spawnInterval = Random.Range(3, 5f);
+        InvokeRepeating("SpawnCoins", spawnDelay, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// 
+    /// </summary>
+    void SpawnCoins()
     {
-        
+        int randomization = Random.Range(0, CoinPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), yPos, Random.Range(spawnLimitZDown, spawnLimitZUp));
+
+        Instantiate(CoinPrefabs[randomization], spawnPos, CoinPrefabs[randomization].transform.rotation);
     }
 }
